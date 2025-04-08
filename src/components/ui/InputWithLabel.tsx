82424cd8ef4d1751dd42809c821750d5a7f1b7a1
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-08 11:37:48 -0300

    refactor: modifica o componente para permitir que sejam exibidas mensagens de erro abaixo dos inputs

--- src/components/ui/InputWithLabel.tsx
+++ src/components/ui/InputWithLabel.tsx
@@ -3,15 +3,17 @@ import React from "react";
 interface InputWithLabelProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  error?: string | undefined;
 }
 
-const InputWithLabel = ({ label, ...props }: InputWithLabelProps) => {
+const InputWithLabel = ({ label, error, ...props }: InputWithLabelProps) => {
   return (
     <React.Fragment>
       <label htmlFor={props.id} className="label">
         {label}
       </label>
       <input {...props} />
+      {error && <span className="error">{error}</span>}
     </React.Fragment>
   );
 };
