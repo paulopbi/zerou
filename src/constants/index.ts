3e9b824bf6f225d91a8159770d62b237841a5c45
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-08 11:39:45 -0300

    feat: adiciona constantes para o projeto

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+//password
+export const maxPasswordLetter = 20;
+export const minPasswordLetter = 6;
+export const maxPasswordErrorMessage = `Pode ter no máximo ${maxPasswordLetter} caracteres!`;
+export const minPasswordErrorMessage = `Precisa ter no mínimo ${minPasswordLetter} caracteres!`;
+
+//username
+export const minUserNameLetter = 3;
+export const userNameErrorMessage = `Nome deve ter pelo menos ${minUserNameLetter} caracteres`;
+
+//email
+export const emailErrorMessage = "Email inválido, tente novamente!";
