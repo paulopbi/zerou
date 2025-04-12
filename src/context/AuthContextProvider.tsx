d1c4ba6f62955de4463c57f829341261d6f891e4
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-12 08:04:04 -0300

    chore: adiciona o login no contexto

--- src/context/AuthContextProvider.tsx
+++ src/context/AuthContextProvider.tsx
@@ -1,16 +1,17 @@
 import { signOut } from "firebase/auth";
 import { useRegister } from "../hooks/useRegister";
-import { useAuthObeserver } from "../hooks/useAuthObserver";
+import { useAuthObserver } from "../hooks/useAuthObserver";
 import { auth } from "../firebase/firebaseConfig";
 import { AuthContext } from "./AuthContext";
+import { useLogin } from "../hooks/useLogin";
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const logout = () => signOut(auth);
+  const { login } = useLogin();
   const { register } = useRegister();
-  const { user, loading } = useAuthObeserver();
-
+  const { user, loading } = useAuthObserver();
   return (
-    <AuthContext.Provider value={{ user, register, logout, loading }}>
+    <AuthContext.Provider value={{ user, register, logout, login, loading }}>
       {children}
     </AuthContext.Provider>
   );
