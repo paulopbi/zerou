e7f8baa4c72a0e5b830092f405912881b8acedf5
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-12 00:18:22 -0300

    fix: componentes movidos para a pasta correta

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useAuth } from "../../hooks/useAuth";
+import { useAuth } from "../hooks/useAuth";
 import { Navigate } from "react-router";
 
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
