98a3df2f8c44b7077648c645208efd5dd27e588f
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-18 10:09:29 -0300

    refactor: substitui GameCard.module.css por GameCard.css, atualiza a estrutura do componente GameCard para suportar imagens e títulos de forma mais flexível

--- src/components/ui/card/GameCard.tsx
+++ src/components/ui/card/GameCard.tsx
@@ -1,34 +1,41 @@
-import styles from "./GameCard.module.css";
+import "./GameCard.css";
 import Button from "../Button";
 import BadgeStatus from "../badge/BadgeStatus";
 import BadgePlatform from "../badge/BadgePlatform";
 
 interface IGame {
   title: string;
-  imageSource: string;
+  imageUrl: string | null;
   platform: string;
   status: string;
 }
 
-const GameCard = ({ title, status, imageSource, platform }: IGame) => {
-  const truncateText = title.substring(0, 20).concat("...");
+const GameCard = ({ title, status, imageUrl, platform }: IGame) => {
+  const truncateText =
+    title.length < 20 ? title : title.substring(0, 20).concat("...");
   return (
-    <div className={styles.cardContainer}>
-      <div className={styles.cardImageContainer}>
-        <img src={imageSource} alt={title} />
-      </div>
+    <div className="game-card-container">
+      {imageUrl ? (
+        <div className="game-card-container--with-image">
+          <img src={imageUrl} alt={title} />
+        </div>
+      ) : (
+        <div className="game-card-container--without-image">
+          <span>{title}</span>
+        </div>
+      )}
 
       <div>
-        <h2 className={styles.cardTitle}>{truncateText}</h2>
+        <h2 className="game-card__title">{truncateText}</h2>
       </div>
 
-      <div className={styles.cardInfos}>
+      <div className="game-card-info">
         <BadgeStatus status={status}>{status}</BadgeStatus>
         <BadgePlatform platform={platform}>{platform}</BadgePlatform>
       </div>
 
       <div>
-        <Button>Ver detalhes</Button>
+        <Button variant="ghost">Ver detalhes</Button>
       </div>
     </div>
   );
