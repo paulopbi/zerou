6a88b954bd38cbd1aab7fd10d830770d3d295243
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-18 12:02:22 -0300

    feat: adiciona estilos CSS para diferentes plataformas no componente BadgePlatform e atualiza a lógica de renderização para utilizar as novas classes

--- src/components/ui/badge/BadgePlatform.tsx
+++ src/components/ui/badge/BadgePlatform.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./BadgePlatform.module.css";
+import "./BadgePlatform.css";
 
 interface IBadgePlatform {
   platform: string;
@@ -8,31 +8,44 @@ interface IBadgePlatform {
 
 const BadgePlatform = ({ children, platform }: IBadgePlatform) => {
   const normalizePlatformText = platform.trim().toLocaleLowerCase();
+  if (normalizePlatformText === "pc")
+    return (
+      <div className="badge-base pc">
+        <span>{children}</span>
+      </div>
+    );
 
   if (normalizePlatformText === "xbox")
     return (
-      <div className={`badge-base ${styles.xbox}`}>
+      <div className="badge-base xbox">
         <span>{children}</span>
       </div>
     );
 
   if (normalizePlatformText === "playstation")
     return (
-      <div className={`badge-base ${styles.playstation}`}>
+      <div className="badge-base playstation">
+        <span>{children}</span>
+      </div>
+    );
+
+  if (normalizePlatformText === "mobile")
+    return (
+      <div className="badge-base mobile">
         <span>{children}</span>
       </div>
     );
 
   if (normalizePlatformText === "nintendo")
     return (
-      <div className={`badge-base ${styles.nintendo}`}>
+      <div className="badge-base nintendo">
         <span>{children}</span>
       </div>
     );
-
-  if (normalizePlatformText === "pc")
+  console.log(normalizePlatformText);
+  if (normalizePlatformText === "steam-deck")
     return (
-      <div className={`badge-base ${styles.pc}`}>
+      <div className="badge-base steam-deck">
         <span>{children}</span>
       </div>
     );
