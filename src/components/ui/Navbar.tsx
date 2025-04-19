3b698ba8592a7718151633dee3a45830299a7268
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-18 22:46:08 -0300

    feat: adiciona estilos CSS para o componente Navbar, incluindo layout, formatação de botões e estrutura de navegação

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,4 +1,5 @@
-import styles from "./Navbar.module.css";
+import "./Navbar.css";
+import React from "react";
 import { Link } from "react-router";
 import { LogOut } from "lucide-react";
 import { useAuth } from "../../hooks/useAuth";
@@ -6,48 +7,57 @@ import { useAuth } from "../../hooks/useAuth";
 const Navbar = () => {
   const { user, logout } = useAuth();
   return (
-    <header className={styles.heading}>
-      <nav className={`${styles.nav} container`}>
-        <Link to="/" className={styles.logo}>
+    <header className="navbar-heading">
+      <nav className="container nav">
+        <Link to="/" className="logo">
           Zerou
         </Link>
-        <ul className={styles.navList}>
-          {user && (
+
+        <nav className="nav">
+          <ul>
             <li>
-              <Link to="/" className="link">
-                Inicio
+              <Link to="/sobre" className="link">
+                Sobre
               </Link>
             </li>
-          )}
-          <li>
-            <Link to="/sobre" className="link">
-              Sobre
-            </Link>
-          </li>
-          {user && (
-            <button
-              title="Clique para sair"
-              className={styles.logout}
-              onClick={logout}
-            >
-              <LogOut />
-            </button>
-          )}
-          {!user && (
-            <li>
-              <Link to="/login" className="link">
-                Fazer Login
-              </Link>
-            </li>
-          )}
-          {!user && (
-            <li>
-              <Link to="/criar-conta" className="link">
-                Criar Conta
-              </Link>
-            </li>
-          )}
-        </ul>
+
+            {/* if user exist */}
+            {user && (
+              <React.Fragment>
+                <li>
+                  <Link to="/" className="link">
+                    Inicio
+                  </Link>
+                </li>
+                <li>
+                  <button
+                    title="Clique para sair"
+                    className="nav-logout__btn"
+                    onClick={logout}
+                  >
+                    <LogOut size={24} />
+                  </button>
+                </li>
+              </React.Fragment>
+            )}
+
+            {/* if user don't exist */}
+            {!user && (
+              <React.Fragment>
+                <li>
+                  <Link to="/login" className="link">
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/criar-conta" className="link">
+                    Criar Conta
+                  </Link>
+                </li>
+              </React.Fragment>
+            )}
+          </ul>
+        </nav>
       </nav>
     </header>
   );
