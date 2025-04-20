fc32ab748e4729cc693e1e28aa513ea47600c986
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-20 20:45:07 -0300

    feat: Atualiza o componente App para incluir o AuthProvider e o Router, substituindo o conteúdo de teste por uma estrutura de roteamento.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Router from "@/routes/Router";
+import { AuthProvider } from "@/contexts/AuthContext";
+
 function App() {
-  return <div>teste</div>;
+  return (
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
+  );
 }
 
 export default App;
