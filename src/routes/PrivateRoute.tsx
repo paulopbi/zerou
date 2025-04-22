cee5a29bd2a159643a97da4383ee36281fdb19a7
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-22 12:05:17 -0300

    feat: Substitui texto de carregamento por componente Loading para melhor experiência do usuário

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,3 +1,4 @@
+import Loading from "@/components/Loading";
 import { useAuth } from "@/contexts/AuthContext";
 import { ReactNode } from "react";
 import { Navigate } from "react-router";
@@ -6,7 +7,7 @@ const PrivateRoute = ({ children }: { children: ReactNode }) => {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return <p>Carregando...</p>;
+    return <Loading />;
   }
 
   return user ? children : <Navigate to="/login" />;
