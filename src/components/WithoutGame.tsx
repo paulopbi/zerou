eed267ac99125957dd910f24cb853c2359c6453a
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-22 12:05:34 -0300

    feat: Adiciona componente WithoutGame com estilos para exibir estado vazio da lista de jogos

---
+++ src/components/WithoutGame.tsx
@@ -0,0 +1,22 @@
+import "./WithoutGame.css";
+import { Plus } from "lucide-react";
+import { Link } from "react-router";
+
+const WithoutGame = () => {
+  return (
+    <div className="empty-state">
+      <h6 className="empty-state__title">Sua lista de jogos está vazia</h6>
+      <p className="empty-state__message">
+        Adicione jogos a sua lista no botão abaixo.
+      </p>
+      <Link
+        to="/adicionar"
+        className="empty-state__button button button--default"
+      >
+        Adicionar Jogo <Plus width={24} height={24} />
+      </Link>
+    </div>
+  );
+};
+
+export default WithoutGame;
