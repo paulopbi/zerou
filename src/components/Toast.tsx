86f53f9c6460cb6f88b3a49652005ba4a5ce0eff
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-24 17:22:56 -0300

    feat: adiciona animacoes com motion

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,6 +1,7 @@
 import "./Toast.css";
 import { ReactNode } from "react";
 import { CircleAlert, CircleCheck, CircleX, Info } from "lucide-react";
+import { easeIn, motion } from "motion/react";
 
 type ToastPropsType = {
   children: ReactNode;
@@ -16,11 +17,16 @@ const Toast = ({ children, variant = "success" }: ToastPropsType) => {
   };
 
   return (
-    <div className={`toast toast--${variant}`}>
+    <motion.div
+      className={`toast toast--${variant}`}
+      initial={{ opacity: 0, scale: 0.9, marginBottom: "-100px" }}
+      animate={{ scale: 1, opacity: 1, marginBottom: "initial" }}
+      transition={{ ease: easeIn }}
+    >
       <span>
         {icons[variant]} {children}
       </span>
-    </div>
+    </motion.div>
   );
 };
 
