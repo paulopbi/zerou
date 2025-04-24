3b8e139250cf8ddbe363ab6110155b26c3a22e79
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-24 16:22:22 -0300

    refactor: atualiza a tipagem dos dados retornados da coleção Firestore, substituindo IDatabaseSchema por DatabaseSchemaType

--- src/services/getFirestoreCollection.ts
+++ src/services/getFirestoreCollection.ts
@@ -1,15 +1,14 @@
 import { db } from "@/config/firebase";
-import { IDatabaseSchema } from "@/types";
+import { DatabaseSchemaType } from "@/types";
 import { collection, getDocs } from "firebase/firestore";
 
 export const getFirestoreCollection = async (path: string) => {
   const gamesCollectionRef = collection(db, path);
   const snapShot = await getDocs(gamesCollectionRef);
 
-  const firestoreData = snapShot.docs.map((doc) => ({
-    id: doc.id,
-    ...(doc.data() as IDatabaseSchema),
-  }));
+  const firestoreData = snapShot.docs.map(
+    (doc) => doc.data() as DatabaseSchemaType
+  );
 
   return firestoreData;
 };
