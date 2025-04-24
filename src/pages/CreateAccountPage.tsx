263588be6020b80052bf5e1594a9d050129819a5
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-24 17:10:55 -0300

    refactor: melhora a lógica de validação e feedback na página de criação de conta, substitui mensagens de erro por sistema de mensagens e ajusta o estado de carregamento

--- src/pages/CreateAccountPage.tsx
+++ src/pages/CreateAccountPage.tsx
@@ -1,14 +1,13 @@
 import "./CreateAccountPage.css";
 import { Link, useNavigate } from "react-router";
 import { FormEvent, useState } from "react";
-import { useAuth } from "@/contexts/AuthContext";
 import {
   createUserWithEmailAndPassword,
   sendEmailVerification,
   updateProfile,
 } from "firebase/auth";
 import { auth } from "@/config/firebase";
-import { passwordMinLenght } from "@/contants";
+import { PASSWORD_MIN_LENGTH, TIMEOUT_VALUE } from "@/contants";
 import { firebaseErrorHandler } from "@/utils/firebaseErrorHandler";
 import { FirebaseError } from "firebase/app";
 import { ToastType } from "@/types";
@@ -20,34 +19,43 @@ const CreateAccountPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [systemMessage, setSystemMessage] = useState<ToastType>({
     message: "",
     variant: null,
   });
 
   const navigate = useNavigate();
-  const { loading } = useAuth();
 
   const handleCreateAccount = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setError("");
+    setIsLoading(true);
+    setSystemMessage({ message: "", variant: null });
 
     if (!displayName || !email || !password || !confirmPassword) {
-      setError("Todos os campos precisam ser preenchidos.");
+      setSystemMessage({
+        message: "Todos os campos precisam ser preenchidos.",
+        variant: "danger",
+      });
       return;
     }
 
     if (
-      password.length < passwordMinLenght ||
-      confirmPassword.length < passwordMinLenght
+      password.length < PASSWORD_MIN_LENGTH ||
+      confirmPassword.length < PASSWORD_MIN_LENGTH
     ) {
-      setError(`A senha precisa ter mais de ${passwordMinLenght} caracteres.`);
+      setSystemMessage({
+        message: `A senha precisa ter mais de ${PASSWORD_MIN_LENGTH} caracteres.`,
+        variant: "danger",
+      });
       return;
     }
 
     if (password !== confirmPassword) {
-      setError("As senhas precisam ser iguais!");
+      setSystemMessage({
+        message: "As senhas precisam ser iguais!",
+        variant: "danger",
+      });
       return;
     }
 
@@ -61,15 +69,11 @@ const CreateAccountPage = () => {
       await sendEmailVerification(newUser.user);
       await updateProfile(newUser.user, { displayName });
 
-      if (!newUser.user) {
-        setError("Algo deu errado! Tente novamente.");
-        return;
-      }
-
       setSystemMessage({
         message: "Usuário criado com sucesso!",
         variant: "success",
       });
+
       setDisplayName("");
       setEmail("");
       setPassword("");
@@ -77,17 +81,26 @@ const CreateAccountPage = () => {
 
       setTimeout(() => {
         setSystemMessage({ message: "", variant: null });
+        setIsLoading(false);
         navigate("/login");
-      }, 2000);
+      }, TIMEOUT_VALUE);
     } catch (error) {
       if (error instanceof FirebaseError) {
         const firebaseReturnErrorMessage = firebaseErrorHandler(error);
-        setError(firebaseReturnErrorMessage);
-        console.error(error);
+        setSystemMessage({
+          message: firebaseReturnErrorMessage,
+          variant: "danger",
+        });
+        console.error("Algo deu errado: " + error);
+        setIsLoading(false);
         return;
       }
       console.error("Algo deu errado: " + error);
-      setError("Algo deu errado tente novamente mais tarde!");
+      setSystemMessage({
+        message: "Algo deu errado tente novamente mais tarde!",
+        variant: "danger",
+      });
+      setIsLoading(false);
     }
   };
 
@@ -132,15 +145,9 @@ const CreateAccountPage = () => {
           className="create-account__input"
         />
 
-        {error && <p className="message-error text-left">{error}</p>}
-
-        {loading ? (
-          <Button type="submit" disabled={loading}>
-            Criando usuário...
-          </Button>
-        ) : (
-          <Button type="submit">Criar Conta</Button>
-        )}
+        <Button type="submit" disabled={isLoading}>
+          {isLoading ? "Criando Conta..." : "Criar Conta"}
+        </Button>
       </form>
 
       <p>
