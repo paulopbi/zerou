f08b1379b3464bd7a1b232f46f903f7996eb7dab
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-24 11:02:19 -0300

    refactor: Atualiza a função getFirestoreCollection para incluir o ID dos documentos retornados

--- src/services/getFirestoreCollection.ts
+++ src/services/getFirestoreCollection.ts
@@ -6,9 +6,10 @@ export const getFirestoreCollection = async (path: string) => {
   const gamesCollectionRef = collection(db, path);
   const snapShot = await getDocs(gamesCollectionRef);
 
-  const firestoreData = snapShot.docs.map(
-    (doc) => doc.data() as IDatabaseSchema
-  );
+  const firestoreData = snapShot.docs.map((doc) => ({
+    id: doc.id,
+    ...(doc.data() as IDatabaseSchema),
+  }));
 
   return firestoreData;
 };
