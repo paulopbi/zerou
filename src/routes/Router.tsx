2af0b2cfabf92646eb2bc3ed5b88077eca51115f
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-24 11:04:14 -0300

    feat: Adiciona rotas privadas para HomePage, AddGamePage e DetailedGame, incluindo tratamento de rotas não encontradas

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import CreateAccountPage from "@/pages/CreateAccountPage";
 import LoginPage from "@/pages/LoginPage";
+import HomePage from "@/pages/HomePage";
+import PrivateRoute from "./PrivateRoute";
+import AddGamePage from "@/pages/AddGamePage";
+import DetailedGame from "@/pages/DetailedGame";
 
 export default function Router() {
   return (
@@ -8,6 +12,31 @@ export default function Router() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/criar-conta" element={<CreateAccountPage />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/adicionar"
+          element={
+            <PrivateRoute>
+              <AddGamePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/jogo/:id"
+          element={
+            <PrivateRoute>
+              <DetailedGame />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<h1>Não existe</h1>} />
       </Routes>
     </BrowserRouter>
   );
