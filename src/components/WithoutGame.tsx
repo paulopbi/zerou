f5f7abc54d394acfc34c5d0bde5f11ce44fb4e66
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-26 16:22:58 -0300

    feat: adiciona foco automático no link de adicionar jogo e ajusta estilos de texto

--- src/components/WithoutGame.tsx
+++ src/components/WithoutGame.tsx
@@ -1,17 +1,26 @@
 import "./WithoutGame.css";
 import { Plus } from "lucide-react";
+import { useEffect, useRef } from "react";
 import { Link } from "react-router";
 
 const WithoutGame = () => {
+  const addGameLinkRef = useRef<null | HTMLAnchorElement>(null);
+  useEffect(() => {
+    if (addGameLinkRef.current) {
+      addGameLinkRef.current.focus();
+    }
+
+    return () => {};
+  }, []);
+
   return (
     <div className="empty-state">
-      <h6 className="empty-state__title">Sua lista de jogos está vazia</h6>
-      <p className="message-info--dark">
-        Adicione jogos a sua lista no botão abaixo.
-      </p>
+      <h6 className="color-info--dark">Sua lista de jogos está vazia</h6>
+      <p>Adicione jogos a sua lista no botão abaixo.</p>
       <Link
         to="/adicionar"
         className="empty-state__button button button--default"
+        ref={addGameLinkRef}
       >
         Adicionar Jogo <Plus width={24} height={24} />
       </Link>
