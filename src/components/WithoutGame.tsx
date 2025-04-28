208484385c5aaf52c28d7e0614367371748d6cd5
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-28 19:47:45 -0300

    feat: personaliza mensagem exibindo o nome do usuário

--- src/components/WithoutGame.tsx
+++ src/components/WithoutGame.tsx
@@ -1,21 +1,28 @@
 import "./WithoutGame.css";
+import { User } from "firebase/auth";
 import { Plus } from "lucide-react";
 import { useEffect, useRef } from "react";
 import { Link } from "react-router";
 
-const WithoutGame = () => {
+type WithoutGameProps = {
+  user: User | null;
+};
+
+const WithoutGame = ({ user }: WithoutGameProps) => {
   const addGameLinkRef = useRef<null | HTMLAnchorElement>(null);
   useEffect(() => {
     if (addGameLinkRef.current) {
       addGameLinkRef.current.focus();
     }
-
-    return () => {};
   }, []);
 
   return (
     <div className="empty-state">
-      <h6 className="color-info--dark">Sua lista de jogos está vazia</h6>
+      <h6 className="color-info--dark empty-state__title">
+        {user && user.displayName
+          ? `${user.displayName}, sua lista de jogos está vazia.`
+          : "Sua lista de jogos está vazia."}
+      </h6>
       <p>Adicione jogos a sua lista no botão abaixo.</p>
       <Link
         to="/adicionar"
