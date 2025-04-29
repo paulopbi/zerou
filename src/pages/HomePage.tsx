af5ca1195a259cd5653958bc2efc482899eca0c5
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-28 21:44:56 -0300

    feat: refatora a HomePage para utilizar o hook useGameData, simplificando a lógica de carregamento e exclusão de jogos

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,87 +1,23 @@
 import "./HomePage.css";
 import { PlusIcon } from "lucide-react";
-import { useEffect, useState } from "react";
 import { Link } from "react-router";
-import { getFirestoreCollection } from "@/utils/getFirestoreCollection";
 import { useAuth } from "@/contexts/AuthContext";
-import { DatabaseSchemaType, ToastType } from "@/types";
-import { deleteDoc, doc } from "firebase/firestore";
-import { db } from "@/config/firebase";
-import { TIMEOUT_TO_REMOVE_TOAST } from "@/constants";
 import Navbar from "@/components/Navbar";
 import WithoutGame from "@/components/WithoutGame";
 import GameCard from "@/components/GameCard";
 import Loading from "@/components/Loading";
 import Toast from "@/components/Toast";
+import useGameData from "@/hooks/useGameData";
 
 const HomePage = () => {
-  const [database, setDatabase] = useState<null | DatabaseSchemaType[]>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [systemMessage, setSystemMessage] = useState<ToastType>({
-    message: "",
-    variant: null,
-  });
   const { user } = useAuth();
-
-  const handleDeleteGame = async (gameID: string) => {
-    setSystemMessage({ message: "", variant: null });
-    try {
-      await deleteDoc(doc(db, "games", gameID));
-      setDatabase((prev) => prev?.filter((game) => game.id !== gameID) || []);
-      setSystemMessage({
-        message: "Jogo excluido com sucesso!",
-        variant: "success",
-      });
-
-      setTimeout(() => {
-        setSystemMessage({ message: "", variant: null });
-      }, TIMEOUT_TO_REMOVE_TOAST);
-    } catch (error) {
-      console.error("Algo deu errado: " + error);
-      setSystemMessage({
-        message: "Algo deu errado ao excluir o jogo, tente novamente!",
-        variant: "danger",
-      });
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setSystemMessage({ message: "", variant: null });
-      setIsLoading(true);
-      try {
-        const databaseResponse = await getFirestoreCollection("games");
-        setDatabase(databaseResponse);
-      } catch (error) {
-        console.error("algo deu errado: " + error);
-        setSystemMessage({
-          message: "Algo deu errado, tente novamente.",
-          variant: "danger",
-        });
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  if (!database || database.length === 0) {
-    return (
-      <>
-        <Navbar />
-        {/* no content */}
-        <section className="container">
-          <WithoutGame user={user} />
-        </section>
-      </>
-    );
-  }
+  const { database, isLoading, systemMessage, setDatabase } = useGameData();
 
   return (
     <>
-      {isLoading && <Loading />}
       <Navbar />
-      <section className="home container">
+
+      <main className="home container">
         <div className="home__heading">
           <p className="home__title">
             {user?.displayName
@@ -93,19 +29,34 @@ const HomePage = () => {
           </Link>
         </div>
 
-        <article className="home__cards">
-          {database.map((game) => (
-            <GameCard
-              key={game.id}
-              gameData={game}
-              deleteGame={handleDeleteGame}
-            />
-          ))}
-        </article>
-      </section>
-      {systemMessage.message && systemMessage.variant && (
-        <Toast variant={systemMessage.variant}>{systemMessage.message}</Toast>
-      )}
+        {(!database || database.length === 0) && (
+          <section className="container">
+            <WithoutGame user={user} />
+          </section>
+        )}
+
+        {database && (
+          <article className="home__cards">
+            {database.map((game) => (
+              <GameCard
+                key={game.id}
+                gameData={game}
+                onDelete={(id) =>
+                  setDatabase(
+                    (prev) => prev?.filter((game) => game.id !== id) || []
+                  )
+                }
+              />
+            ))}
+          </article>
+        )}
+
+        {systemMessage.message && systemMessage.variant && (
+          <Toast variant={systemMessage.variant}>{systemMessage.message}</Toast>
+        )}
+
+        {isLoading && <Loading />}
+      </main>
     </>
   );
 };
