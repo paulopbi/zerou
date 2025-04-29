d008694795ee8c342405f423c198356f6051a333
Paulo Victor Silva <paulpbi16@gmail.com>
2025-04-28 21:44:46 -0300

    feat: integra o hook useDeleteGame ao componente GameCard para gerenciar a exclusão de jogos com feedback visual

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,91 +5,103 @@ import { truncateText } from "@/utils/truncateText";
 import { X } from "lucide-react";
 import { Link } from "react-router";
 import { motion } from "motion/react";
-import Button from "@/components/Button";
-import { useState } from "react";
 import { statusDictionary } from "@/utils/statusDictionary ";
+import Button from "@/components/Button";
+import useDeleteGame from "@/hooks/useDeleteGame";
+import Toast from "./Toast";
 
-interface GameCardProps {
+type GameCardProps = {
   gameData: DatabaseSchemaType;
-  deleteGame: (gameID: string) => void;
-}
+  onDelete: (id: string) => void;
+};
 
-const GameCard = ({ gameData, deleteGame }: GameCardProps) => {
-  const [shouldAnimateWhenRemove, setShouldAnimateWhenRemove] = useState(false);
-  const handleClick = () => {
-    setShouldAnimateWhenRemove(true);
-    setTimeout(() => {
-      deleteGame(gameData.id);
-    }, 500);
-  };
+const cardVariants = {
+  initial: { opacity: 0, y: -50 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -50 },
+  hover: {
+    y: -20,
+    scale: 1.02,
+    transition: { type: "spring", stiffness: 300 },
+  },
+};
 
-  const cardVariants = {
-    initial: { opacity: 0, y: -50 },
-    animate: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: -50 },
-    hover: {
-      y: -20,
-      scale: 1.02,
-      transition: { type: "spring", stiffness: 300 },
-    },
+const GameCard = ({ gameData, onDelete }: GameCardProps) => {
+  const { handleDelete, isDeleted, systemMessage } = useDeleteGame(
+    gameData.id,
+    "games"
+  );
+
+  const handleDeleteClick = async () => {
+    await handleDelete();
+    onDelete(gameData.id);
   };
 
   return (
-    <motion.div
-      variants={cardVariants}
-      initial="initial"
-      whileHover="hover"
-      animate={shouldAnimateWhenRemove ? "exit" : "animate"}
-      transition={{ duration: 0.5 }}
-      className="game-card"
-    >
-      <div className="game-card__controllers">
-        <Button
-          className="game-card__controllers-button"
-          onClick={handleClick}
-          title="Ao clicar, o jogo será excluído."
-        >
-          <X width={22} height={22} color="var(--color-danger-dark)" />
-        </Button>
-      </div>
+    <>
+      <motion.div
+        variants={cardVariants}
+        initial="initial"
+        whileHover="hover"
+        animate={isDeleted ? "exit" : "animate"}
+        transition={{ duration: 0.5 }}
+        className="game-card"
+      >
+        <div className="game-card__controllers">
+          <Button
+            className="game-card__controllers-button"
+            title="Ao clicar, o jogo será excluído."
+            onClick={handleDeleteClick}
+          >
+            <X width={22} height={22} color="var(--color-danger-dark)" />
+          </Button>
+        </div>
 
-      <div className="game-card__image-container">
-        {gameData.image_source ? (
-          <img
-            className="game-card__image-picture"
-            src={gameData.image_source}
-            alt={`Foto do Jogo ${gameData.title}`}
-            loading="lazy"
-          />
-        ) : (
-          <div className="game-card__image-label">
-            <span className="game-card__image-label-text">
-              {truncateText(gameData.title, 40)}
+        <div className="game-card__image-container">
+          {gameData.image_source ? (
+            <img
+              className="game-card__image-picture"
+              src={gameData.image_source}
+              alt={`Foto do Jogo ${gameData.title}`}
+              loading="lazy"
+            />
+          ) : (
+            <div className="game-card__image-label">
+              <span className="game-card__image-label-text">
+                {truncateText(gameData.title, 40)}
+              </span>
+            </div>
+          )}
+        </div>
+
+        <div className="game-card__content">
+          <h6 className="game-card__title">
+            {truncateText(gameData.title, 26)}
+          </h6>
+          <div className="game-card__badges">
+            <span
+              className={`badge badge--${getBadgeModifier(gameData.status)}`}
+            >
+              {statusDictionary(gameData.status)}
+            </span>
+            <span
+              className={`badge badge--${getBadgeModifier(gameData.platform)}`}
+            >
+              {gameData.platform}
             </span>
           </div>
-        )}
-      </div>
-
-      <div className="game-card__content">
-        <h6 className="game-card__title">{truncateText(gameData.title, 26)}</h6>
-        <div className="game-card__badges">
-          <span className={`badge badge--${getBadgeModifier(gameData.status)}`}>
-            {statusDictionary(gameData.status)}
-          </span>
-          <span
-            className={`badge badge--${getBadgeModifier(gameData.platform)}`}
+          <Link
+            to={`/jogo/${gameData.id}`}
+            className="game-card__button button button--ghost"
           >
-            {gameData.platform}
-          </span>
+            Ver Detalhes
+          </Link>
         </div>
-        <Link
-          to={`/jogo/${gameData.id}`}
-          className="game-card__button button button--ghost"
-        >
-          Ver Detalhes
-        </Link>
-      </div>
-    </motion.div>
+      </motion.div>
+      {systemMessage.message && systemMessage.variant && (
+        <Toast variant={systemMessage.variant}>{systemMessage.message}</Toast>
+      )}
+    </>
   );
 };
 
